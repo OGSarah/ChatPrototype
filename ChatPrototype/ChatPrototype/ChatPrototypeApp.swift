8775ac8ee8235557002a1b37daed0a250a8bfe6a
Sarah Clark <sarahireneclark@gmail.com>
2025-08-19 13:25:05 -0500

    Initial commit of project

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Sarah Clark on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
